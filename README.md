e9510caf62db68694d30dc094107bb2d87a024c8
euvinkeel <emkeel@ucsd.edu>
2021-11-11 21:49:42 -0800

    Complete Explore

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Lab 8 - Starter
 
+## Partners: Euvin Keel, Caiwei Xiao
+
 1. Where would you fit your automated tests in your Recipe project development pipeline?
 
     I would fit the automated tests within a Github action in order to make sure the latest code doesn't break any unexpected functionality. We shouldn't worry about running them manually as the sole filter (although it would probably be encouraged before pushing code), and we certainly wouldn't wait until the very end of development to test the code.
 
-2. No, it seems excessive since checking the output of a function won't require emulating user actions.
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
 
+    No, it seems excessive since checking the output of a function won't require emulating user actions.
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+    No, since we cannot test how these individual functions or components interact with each other on the application level (which could include checking to see if messages display properly, are styled properly, if the other person recieves it in their chatbox, etc)
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+    Yes, since a simple comparison of a message length would probably only require a single function call.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,84 @@
 // unit.test.js
 
+const { TestWatcher } = require('jest');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
+/*
+
+Create 2 tests that should be true, and 2 tests that should be false for each function (4 tests x 5 functions = 20 tests).
+For clarification, all of your tests should pass, but you should expect two of your tests to return a true response and two of your tests to return a false response
+e.g. expect(2+2).toBe(true) and expect(4+4).toBe(10)
+
+*/
+
 // TODO - Part 2
+
+// tests for phonenumber
+test('phoneNumber should recognize a valid phone number', () => {
+    expect(functions.isPhoneNumber("(123)243-1234")).toBe(true); 
+});
+test('phoneNumber should recognize another valid phone number', () => {
+    expect(functions.isPhoneNumber("(000)000-0101")).toBe(true); 
+});
+test('phoneNumber shouldn\'t recognize invalid phone numbers', () => {
+    expect(functions.isPhoneNumber("(x23)")).toBe(false); 
+});
+test('phoneNumber shouldn\'t recognize invalid phone numbers (again)', () => {
+    expect(functions.isPhoneNumber("(abc)mcd-0101")).toBe(false); 
+}); 
+
+// tests for emails
+test('isEmail should recognize a valid email', () => {
+    expect(functions.isEmail("somecoolguy@gmail.com")).toBe(true); 
+});
+test('isEmail should recognize another valid email', () => {
+    expect(functions.isEmail("student@ucsd.edu")).toBe(true); 
+});
+test('isEmail shouldn\'t recognize invalid emails', () => {
+    expect(functions.isEmail("klasdjfklsadjf")).toBe(false); 
+});
+test('isEmail shouldn\'t recognize invalid emails (again)', () => {
+    expect(functions.isEmail("dhjlkasjdklas@something")).toBe(false); 
+}); 
+
+// tests for passwords
+test('isStrongPassword should recognize a valid password', () => {
+    expect(functions.isStrongPassword("A2390d_")).toBe(true); 
+});
+test('isStrongPassword should recognize another valid password', () => {
+    expect(functions.isStrongPassword("Woodjhf35")).toBe(true); 
+});
+test('isStrongPassword shouldn\'t recognize super short passwords', () => {
+    expect(functions.isStrongPassword("hi")).toBe(false); 
+});
+test('isStrongPassword shouldn\'t recognize super long passwords', () => {
+    expect(functions.isStrongPassword("d98fh834h83f48hg43948g34g433434")).toBe(false); 
+}); 
+
+// tests for dates
+test('isDate should recognize a valid date', () => {
+    expect(functions.isDate("01/01/2000")).toBe(true); 
+});
+test('isDate should recognize another valid date', () => {
+    expect(functions.isDate("1/1/2000")).toBe(true); 
+});
+test('isDate shouldn\'t recognize invalid dates', () => {
+    expect(functions.isDate("2000/1/1")).toBe(false); 
+});
+test('isDate shouldn\'t recognize invalid dates 2', () => {
+    expect(functions.isDate("2")).toBe(false); 
+}); 
+
+// tests for hex colors
+test('isHexColor should recognize a valid hex color', () => {
+    expect(functions.isHexColor("FFF")).toBe(true); 
+});
+test('isHexColor should recognize another valid hex color', () => {
+    expect(functions.isHexColor("#AB23FE")).toBe(true); 
+});
+test('isHexColor shouldn\'t recognize invalid hex colors', () => {
+    expect(functions.isHexColor("ZWFFFF")).toBe(false); 
+});
+test('isHexColor shouldn\'t recognize invalid hex colors again', () => {
+    expect(functions.isHexColor("#FFFF")).toBe(false); 
+}); 
